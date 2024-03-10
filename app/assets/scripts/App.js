2ae3d5530b255ee3398e0979a7f3dfe7246bd16a
Christopher Wells <wellswebtech@gmil.com>
2024-03-10 02:27:32 +0200

    Completed header, some still using old code and some using new grid

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,3 +4,16 @@ import '../styles/styles.css'
 if (module.hot) {
     module.hot.accept()
 }
+
+/* Lesson example code below this line */
+console.log("This is a test!");
+console.log("Hello, my name is John Doe and my favourite color is blue.");
+console.log("Hello, my name is Jane Smith and my favourite color is green.");
+
+function person(name, favColor) {
+    console.log("Hello, my name is " + name + " and my favourite color is " + favColor + ".");
+}
+console.log("Making use of function call in next lines..");
+
+person("John Doe", "blue");
+person("Jane Smith", "green");
